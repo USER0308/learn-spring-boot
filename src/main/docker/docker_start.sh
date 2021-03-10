c138dc593b12d43b5f60434a830b51d94c69db36
USER0308 <xiaojian.liang@yandex.com>
2021-03-10 20:24:16 +0800

    deploy: update docker-compose.yaml file
    update docker-compose.yaml file, add rabbitmq dependency

--- src/main/docker/docker_start.sh
+++ src/main/docker/docker_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo docker run -d -p 6379:6379 redis:6.2.0
-sudo docker run -d -p 8033:8033 learn-spring-boot:1.0.0
-sudo docker ps |grep learn-spring-boot
+sudo docker run -d --name nginx -p 80:80 -v /home/kexin/Data/nginx/config/:/etc/nginx/ -v /home/kexin/Data/nginx/logs/:/var/log/nginx nginx:1.12.2
+sudo docker run -d --name rabbitmq -p 5672:5672 -p 15672:15672 -v /home/kexin/Data/rabbitmq/data:/var/lib/rabbitmq/ -e RABBITMQ_DEFAULT_USER=admin -e RABBITMQ_DEFAULT_PASS=admin rabbitmq:3.8-management
+sudo docker run -d --privileged=true -p 6379:6379 --restart always -v /home/kexin/Data/redis/config/:/etc/redis/ -v /home/kexin/Data/redis/data:/data --name redis redis:6.2.0
