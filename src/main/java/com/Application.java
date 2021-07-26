22a478998c92d2a2fd81312481b8c148a673d67a
USER0308 <xiaojian.liang@yandex.com>
2021-07-26 22:04:50 +0800

    Eureka

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -1,12 +1,12 @@
 package com;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableEurekaClient
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
