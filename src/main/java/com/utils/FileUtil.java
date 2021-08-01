d7fc45693c08d369d5b816e79efdd2e4bb713bb7
USER0308 <xiaojian.liang@yandex.com>
2021-08-01 23:53:37 +0800

    IO

--- src/main/java/com/utils/FileUtil.java
+++ src/main/java/com/utils/FileUtil.java
@@ -1,4 +1,14 @@
 package com.utils;
 
+import java.io.InputStream;
+
 public class FileUtil {
+
+    public static void writeFile(String filePath, String content) {
+
+    }
+    
+    public static void writeFile(String filePath, InputStream inputStream) {
+
+    }
 }
